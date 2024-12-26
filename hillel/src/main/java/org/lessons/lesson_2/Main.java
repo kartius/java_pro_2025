ae43289d2c8c2d843116cf2293645fbf00d491f1
mykola.tsyb <mykola.tsyb@oracle.com>
2024-12-26 18:51:30 +0100

    second commit

--- hillel/src/main/java/org/lessons/lesson_2/Main.java
+++ hillel/src/main/java/org/lessons/lesson_2/Main.java
@@ -3,5 +3,6 @@ package org.lessons.lesson_2;
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
+    System.out.println("Second commit");
   }
 }
